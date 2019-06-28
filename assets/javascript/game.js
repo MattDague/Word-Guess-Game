41a586f4338bba3cbb326fe5eea2a6c4972052a8
MattDague <mattdague@gmail.com>
2019-06-27 23:31:26 -0400

    coded js to take word input and check it against correct answer, also checks word length to add blank spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,117 @@
 var userGuess
-var characters = ["Audrey", "Ben", "Bob", "Bobby", "Coop", "Evil Coop", "Fireman", "Gordon", "Laura", "Leland", "Log Lady", "Shelly"]
+var characters = ["AudreyHorne", "BenjaminHorne", "Bob", "BobbyBriggs", "DaleCooper", "EvilCoop", "TheFireman", "GordonCole", "LauraPalmer", "LelandPalmer", "LogLady", "ShellyJohnson"]
+var audrey = ["a", "u", "d", "r", "e", "y"]
+var blanks = []
 
 
-
-document.onkeyup = function(event) {
-
-var userGuess = event.key;
+// determines random character
 var actual = characters[Math.floor(Math.random() * characters.length)];
 
-document.getElementById("current").innerHTML = actual;
+console.log(actual);
 
 
-console.log(actual);}h
+// places - marks based on amount of letters in characters name
+for ( i=0; i < actual.length; i++) {
+    blanks.push('-'); 
+      
+};
+
+
+// checks user input
+document.onkeyup = function(event) {
+var userGuess = event.key;
+document.getElementById("current").innerHTML = blanks.join("");
+console.log(userGuess);
+
+
+//checks if users guess conforms to actual answer
+if (actual.indexOf(userGuess) > -1) {;
+    console.log(true);
+}
+
+else {
+    console.log(false)
+}
+
+
+
+}
+console.log(blanks);
+
+// console.log(actual.length);
+
+// //     return {blanks
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//code for audrey
+// if (actual === characters[0]) {
+
+//     for( i = 0; i < audrey.length; i++) {
+
+//         document.getElementById("current").innerHTML = "- " + "- "+ "- "+ "- "+ "- "+ "- " ;
+//     }
+
+
+
+// };
+// }
+
+// for (i = 0,  i < aud)
+// var letters = actual.charAt(i);
+// console.log(letters);}
+
+
+
+// things to code
+
+
+// 1. code takes var actual and uses char at to determine letters
+
+
+
+
+
+// 2. letters turn into underscores for same amount of total letters
+
+// 3.  game checks userGuess with letters
+
+// 4. game replaces underscore with correct letters
+
+// 5.game places incorrect letters at bottom
+
+// 6. game subtracts number of guesses after wrong answer
+
+// 7. if number of guesses = 0 display lose and reset
+
+// 8. if word is completed display win, display picture
+
+// 9. Make reset button instead?
