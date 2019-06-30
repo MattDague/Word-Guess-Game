7b93c18c43be1ab50252882c23b11883a1e176e5
MattDague <mattdague@gmail.com>
2019-06-29 22:21:22 -0400

    final version, updated readme, cleaned up code and added comments where needed

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# bootstrap-portfolio
+# word guess game!
+
+This is a hangman game set in the Twin Peaks universe! By pressing any letter you make your first attempt at guessing the name of a multitude of characters.You only get 10 wrong letters so you better be familiar. 
+
+Once the game is won or lost the text will change and a reset button will appear to send you back to the start. If you win however you it will give you a notch in the win column. You also get a nice picture of the person you guessed!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,32 +4,20 @@ var wins = 0
 var blanks = []
 
 
-// determines random character
-
-// var actual = characters[Math.floor(Math.random() * characters.length)];
-
-// console.log(actual);
-
-
 function gameStart() {
     var wrongLetters = []
     var blanks = []
+    // determines random character
     var actual = characters[Math.floor(Math.random() * characters.length)];
     var remainingGuesses = 10
-    // places - marks based on amount of letters in characters name
 
-
+// places - marks based on amount of letters in characters name
     for (i = 0; i < actual.length; i++) {
         blanks.push('-');
     };
     console.log(actual);
 
     console.log(blanks);
-
-    // document.onload = function () {
-    //     document.getElementById("current").innerHTML = blanks.join("");
-
-    // }
 
     // checks user input
     document.onkeyup = function (event) {
@@ -52,117 +40,81 @@ function gameStart() {
                     if (blanks.includes("-") == false) {
                         wins++;
                         document.getElementById("wins").innerHTML = wins;
-                        document.getElementById("instructions").innerHTML = "Click restart to play again!";
+                        document.getElementById("instructions").innerHTML = "You win! Click restart to play again!";
                         document.getElementById("restartbutton").style.display = "block";
 
                         //displays character image based on result, there might be a quicker way for this but i couldnt get anything else working 
-                            if (actual == "audreyhorne"){
+                        if (actual == "audreyhorne") {
                             document.getElementById("audrey").style.display = "block";
-                        }   
-                        else if (actual == "benjaminhorne"){
+                        }
+                        else if (actual == "benjaminhorne") {
                             document.getElementById("ben").style.display = "block";
                         }
-                        else if (actual == "bob"){
+                        else if (actual == "bob") {
                             document.getElementById("bob").style.display = "block";
                         }
-                        else if (actual == "bobbybriggs"){
+                        else if (actual == "bobbybriggs") {
                             document.getElementById("bobby").style.display = "block";
                         }
-                        else if (actual == "dalecooper"){
+                        else if (actual == "dalecooper") {
                             document.getElementById("coop").style.display = "block";
                         }
-                        else if (actual == "evilcoop"){
+                        else if (actual == "evilcoop") {
                             document.getElementById("evilcoop").style.display = "block";
                         }
-                        else if (actual == "thefireman"){
+                        else if (actual == "thefireman") {
                             document.getElementById("fireman").style.display = "block";
                         }
-                        else if (actual == "gordoncole"){
+                        else if (actual == "gordoncole") {
                             document.getElementById("gordon").style.display = "block";
                         }
-                        else if (actual == "laurapalmer"){
+                        else if (actual == "laurapalmer") {
                             document.getElementById("laura").style.display = "block";
                         }
-                        else if (actual == "lelandpalmer"){
+                        else if (actual == "lelandpalmer") {
                             document.getElementById("leland").style.display = "block";
                         }
-                        else if (actual == "loglady"){
+                        else if (actual == "loglady") {
                             document.getElementById("loglady").style.display = "block";
                         }
-                        else if (actual == "shellyjohnson"){
+                        else if (actual == "shellyjohnson") {
                             document.getElementById("shelly").style.display = "block";
                         }
 
-
-                        // if(blanks == "audreyhorne"){ 
-                        //     document.getElementById("audrey").style.display = "block";
-                        // }
-
-
-
-
-
-                        // return blanks;
                     }
 
 
                 }
-                
-        // else if (wrongLetters.includes(userGuess) == false) {
-        //     wrongLetters.push(userGuess);
-        //     --remainingGuesses;
-        //     document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
-        //     document.getElementById("remaining").innerHTML = remainingGuesses;
-        //     if (remainingGuesses <= 0) {
-        //         document.getElementById("instructions").innerHTML = "Click restart to play again!";
-        //         document.getElementById("restartbutton").style.display = "block";
-        //     }
+
             }
-
-
-
 
         }
 
-
+        //wrong letters get sent to array at bottom and guesses goes down
         else if (wrongLetters.includes(userGuess) == false) {
             wrongLetters.push(userGuess);
             --remainingGuesses;
             document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
             document.getElementById("remaining").innerHTML = remainingGuesses;
+            //losing conditions
             if (remainingGuesses <= 0) {
-                document.getElementById("instructions").innerHTML = "Click restart to play again!";
+                document.getElementById("instructions").innerHTML = "You Lose! Click restart to play again!";
                 document.getElementById("restartbutton").style.display = "block";
             }
         }
+
+
     }
 
 
-
-
-
-
-
-
-
-    // use for loop and index of to replace dashes with individual letters,  if guess doesnt match put in loss column
-    // you can use indexof() to replace letters!
-    // you can set variables equal to functions
-
-    // you can fix the start by putting it in a function and calling it, function name(){}; name()
-
-    // maybe restart button calls that function again?
-
-
-
-    console.log(wrongLetters);
 };
-
+//resets the games variable to start again
 function endGame() {
     var remainingGuesses = 10
     var wrongLetters = []
     document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
     document.getElementById("remaining").innerHTML = remainingGuesses;
+    document.getElementById("instructions").innerHTML = "Press any letter to get started!";
     //clears images
     document.getElementById("audrey").style.display = "none";
     document.getElementById("ben").style.display = "none";
@@ -177,46 +129,12 @@ function endGame() {
     document.getElementById("loglady").style.display = "none";
     document.getElementById("shelly").style.display = "none";
 
-   
+    //calls function to start game over
     gameStart();
 
 
 };
 
 
-
+// calls game to start
 gameStart();
-
-
-
-
-
-
-
-// things to code
-
-
-// 1. code takes var actual and uses char at to determine letters
-
-
-
-
-
-// 2. letters turn into underscores for same amount of total letters
-
-// 3.  game checks userGuess with letters
-
-// 4. game replaces underscore with correct letters
-
-// 5.game places incorrect letters at bottom
-
-// 6. game subtracts number of guesses after wrong answer
-
-// 7. if number of guesses = 0 display lose and reset
-
-// 8. if word is completed display win, display picture
-
-// 9. Make reset button instead?'
-
-
-// test
