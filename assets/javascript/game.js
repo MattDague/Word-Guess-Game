801d0d73225c3ab211d98145aa690da320611e3a
MattDague <mattdague@gmail.com>
2019-06-29 21:25:01 -0400

    images now displaying on win and clearing on reset, reset button functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,31 @@
 var userGuess
 var characters = ["audreyhorne", "benjaminhorne", "bob", "bobbybriggs", "dalecooper", "evilcoop", "thefireman", "gordoncole", "laurapalmer", "lelandpalmer", "loglady", "shellyjohnson"]
-var remainingGuesses = 10
-var wrongLetters = []
-var blanks = []
 var wins = 0
-console.log(remainingGuesses)
+var blanks = []
+
 
 // determines random character
 
-var actual = characters[Math.floor(Math.random() * characters.length)];
+// var actual = characters[Math.floor(Math.random() * characters.length)];
 
-console.log(actual);
+// console.log(actual);
+
 
 function gameStart() {
+    var wrongLetters = []
+    var blanks = []
+    var actual = characters[Math.floor(Math.random() * characters.length)];
+    var remainingGuesses = 10
     // places - marks based on amount of letters in characters name
+
 
     for (i = 0; i < actual.length; i++) {
         blanks.push('-');
     };
+    console.log(actual);
 
     console.log(blanks);
 
-    ;
     // checks user input
     document.onkeyup = function (event) {
         var userGuess = event.key;
@@ -38,117 +42,156 @@ function gameStart() {
                 if (userGuess === actual[h]) {
                     blanks[h] = userGuess;
                     document.getElementById("current").innerHTML = blanks.join("");
-                    
+
+                    //add to win column if no blanks are left
                     if (blanks.includes("-") == false) {
                         wins++;
                         document.getElementById("wins").innerHTML = wins;
-                        
+                        document.getElementById("instructions").innerHTML = "Click restart to play again!";
+                        document.getElementById("restartbutton").style.display = "block";
+
+                        //displays character image based on result, there might be a quicker way for this but i couldnt get anything else working 
+                            if (actual == "audreyhorne"){
+                            document.getElementById("audrey").style.display = "block";
+                        }   
+                        else if (actual == "benjaminhorne"){
+                            document.getElementById("ben").style.display = "block";
+                        }
+                        else if (actual == "bob"){
+                            document.getElementById("bob").style.display = "block";
+                        }
+                        else if (actual == "bobbybriggs"){
+                            document.getElementById("bobby").style.display = "block";
+                        }
+                        else if (actual == "dalecooper"){
+                            document.getElementById("coop").style.display = "block";
+                        }
+                        else if (actual == "evilcoop"){
+                            document.getElementById("evilcoop").style.display = "block";
+                        }
+                        else if (actual == "thefireman"){
+                            document.getElementById("fireman").style.display = "block";
+                        }
+                        else if (actual == "gordoncole"){
+                            document.getElementById("gordon").style.display = "block";
+                        }
+                        else if (actual == "laurapalmer"){
+                            document.getElementById("laura").style.display = "block";
+                        }
+                        else if (actual == "lelandpalmer"){
+                            document.getElementById("leland").style.display = "block";
+                        }
+                        else if (actual == "loglady"){
+                            document.getElementById("loglady").style.display = "block";
+                        }
+                        else if (actual == "shellyjohnson"){
+                            document.getElementById("shelly").style.display = "block";
+                        }
+
+
+                        // if(blanks == "audreyhorne"){ 
+                        //     document.getElementById("audrey").style.display = "block";
+                        // }
+
+
+
+
+
+                        // return blanks;
                     }
 
-                    //WIP LOSE CONDITION
-                //     else if ((blanks.includes("-") == true) && (remaningGuesses === 0))
-                //     wins--;
-                //     document.getElementById("wins").innerHTML = wins;
-                // }    
-                }
-                
-                    
-                
-
 
                 }
-        
+                
+        // else if (wrongLetters.includes(userGuess) == false) {
+        //     wrongLetters.push(userGuess);
+        //     --remainingGuesses;
+        //     document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
+        //     document.getElementById("remaining").innerHTML = remainingGuesses;
+        //     if (remainingGuesses <= 0) {
+        //         document.getElementById("instructions").innerHTML = "Click restart to play again!";
+        //         document.getElementById("restartbutton").style.display = "block";
+        //     }
+            }
 
-            else if (wrongLetters.includes(userGuess) == false) {
-                wrongLetters.push(userGuess);
-                --remainingGuesses;
-                document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
-                document.getElementById("remaining").innerHTML = remainingGuesses;
-            }
-            }
+
+
+
         }
 
-        //if answer is wrong subtract from remaining guesses
-        // else if (wrongLetters.includes(userGuess) == true) {
-        //     document.getElementById("remaining").innerHTML = remainingGuesses;
-        // }
 
-        //if guess is wrong and is not listed in wrong letters, put it in wrong letters and update remaining guesses
-       
-
-
-
-
-        //game restart
-        // if ((remainingGuesses == 0) || (actual.length == )  {
-        //     gameStart();
-        //     remainingGuesses = 0;
+        else if (wrongLetters.includes(userGuess) == false) {
+            wrongLetters.push(userGuess);
+            --remainingGuesses;
+            document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
+            document.getElementById("remaining").innerHTML = remainingGuesses;
+            if (remainingGuesses <= 0) {
+                document.getElementById("instructions").innerHTML = "Click restart to play again!";
+                document.getElementById("restartbutton").style.display = "block";
+            }
+        }
+    }
 
 
-// use for loop and index of to replace dashes with individual letters,  if guess doesnt match put in loss column
-// you can use indexof() to replace letters!
-// you can set variables equal to functions
-
-// you can fix the start by putting it in a function and calling it, function name(){}; name()
-
-// maybe restart button calls that function again?
 
 
+
+
+
+
+
+    // use for loop and index of to replace dashes with individual letters,  if guess doesnt match put in loss column
+    // you can use indexof() to replace letters!
+    // you can set variables equal to functions
+
+    // you can fix the start by putting it in a function and calling it, function name(){}; name()
+
+    // maybe restart button calls that function again?
+
+
+
+    console.log(wrongLetters);
+};
+
+function endGame() {
+    var remainingGuesses = 10
+    var wrongLetters = []
+    document.getElementById("guessed").innerHTML = wrongLetters.join(" ");
+    document.getElementById("remaining").innerHTML = remainingGuesses;
+    //clears images
+    document.getElementById("audrey").style.display = "none";
+    document.getElementById("ben").style.display = "none";
+    document.getElementById("fireman").style.display = "none";
+    document.getElementById("coop").style.display = "none";
+    document.getElementById("evilcoop").style.display = "none";
+    document.getElementById("bob").style.display = "none";
+    document.getElementById("bobby").style.display = "none";
+    document.getElementById("gordon").style.display = "none";
+    document.getElementById("laura").style.display = "none";
+    document.getElementById("leland").style.display = "none";
+    document.getElementById("loglady").style.display = "none";
+    document.getElementById("shelly").style.display = "none";
+
+    // initial();
+    gameStart();
+
+
+};
+
+
+// function restart() {
+//     endGame()
+
+
+// };
 
 
 
 gameStart();
 
-// console.log(actual.length);
-
-// //     return {blanks
-
-
-// var audrey = ["a", "u", "d", "r", "e", "y"]
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//code for audrey
-// if (actual === characters[0]) {
-
-//     for( i = 0; i < audrey.length; i++) {
-
-//         document.getElementById("current").innerHTML = "- " + "- "+ "- "+ "- "+ "- "+ "- " ;
-//     }
-
-
-
-// };
-// }
-
-// for (i = 0,  i < aud)
-// var letters = actual.charAt(i);
-// console.log(letters);}
 
 
 
@@ -175,4 +218,7 @@ gameStart();
 
 // 8. if word is completed display win, display picture
 
-// 9. Make reset button instead?
+// 9. Make reset button instead?'
+
+
+// test
